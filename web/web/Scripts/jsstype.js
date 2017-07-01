1965833835818ae74359a17f40a38b9fecfda14a
Việt Chức <Vietchucmta11@gmail.com>
2017-07-02 05:36:43 +0700

    webtt

---
+++ web/web/Scripts/jsstype.js
@@ -0,0 +1,79 @@
+﻿
+
+(function () {
+    $('#carouselABC').carousel({ interval: 3600 });
+}());
+
+(function () {
+    $('#carouselABC1').carousel({ interval: 3600 });
+}());
+
+(function () {
+    $('myCarouselWrapper').carousel({ interval: 3600 });
+}());
+
+(function () {
+    $('Link').carousel({ interval: 3600 });
+}());
+
+(function () {
+    $('.carousel-showmanymoveone .item').each(function () {
+        var itemToClone = $(this);
+
+        for (var i = 1; i < 4; i++) {
+            itemToClone = itemToClone.next();
+
+            // wrap around if at end of item collection
+            if (!itemToClone.length) {
+                itemToClone = $(this).siblings(':first');
+            }
+
+            // grab item, clone, add marker class, add to collection
+            itemToClone.children(':first-child').clone()
+                .addClass("cloneditem-" + (i))
+                .appendTo($(this));
+        }
+    });
+}());
+
+(function () {
+    $('.carousel-showmanymoveone1 .item').each(function () {
+        var itemToClone = $(this);
+
+        for (var i = 1; i < 3; i++) {
+            itemToClone = itemToClone.next();
+
+            // wrap around if at end of item collection
+            if (!itemToClone.length) {
+                itemToClone = $(this).siblings(':first');
+            }
+
+            // grab item, clone, add marker class, add to collection
+            itemToClone.children(':first-child').clone()
+                .addClass("cloneditem-" + (i))
+                .appendTo($(this));
+        }
+    });
+}());
+
+(function () {
+    $('.carousel-Body_Link .item').each(function () {
+        var itemToClone = $(this);
+
+        for (var i = 1; i < 5; i++) {
+            itemToClone = itemToClone.next();
+
+            // wrap around if at end of item collection
+            if (!itemToClone.length) {
+                itemToClone = $(this).siblings(':first');
+            }
+
+            // grab item, clone, add marker class, add to collection
+            itemToClone.children(':first-child').clone()
+                .addClass("cloneditem-" + (i))
+                .appendTo($(this));
+        }
+    });
+}());
+
+
